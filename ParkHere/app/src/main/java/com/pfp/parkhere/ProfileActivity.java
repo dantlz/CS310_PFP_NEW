2872a70370c674af3047e4e259d0698b94720a66
Cu Le Huy <lcu@usc.edu>
2016-10-29 17:30:17 -0700

    Edit profile update

--- ParkHere/app/src/main/java/com/pfp/parkhere/ProfileActivity.java
+++ ParkHere/app/src/main/java/com/pfp/parkhere/ProfileActivity.java
@@ -1,60 +1,134 @@
 package com.pfp.parkhere;
 
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.graphics.Color;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-    private android.widget.ImageView mImageView; 
-    private android.widget.EditText mEmail; 
-    private android.widget.EditText mPassword; 
-    private android.widget.EditText mPhone; 
-    private android.widget.EditText mName; 
-    private android.widget.Button mEditButton;
+import static com.pfp.parkhere.R.id.editButton;
+
+public class ProfileActivity extends AppCompatActivity
+{
+
+    protected EditText mEmail, mPassword, mPhone, mName;
+    private Button mEditButton, mSaveButton;
+    private ImageView mImageView = null;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        mImageView = (android.widget.ImageView) findViewById(R.id.imageView); 
-        mEmail = (android.widget.EditText) findViewById(R.id.emailLabel); 
-        mPassword = (android.widget.EditText) findViewById(R.id.passwordLabel); 
-        mPhone = (android.widget.EditText) findViewById(R.id.phoneLabel); 
-        mName = (android.widget.EditText) findViewById(R.id.nameLabel);
+        mImageView = (ImageView)findViewById(R.id.imageView);
+        mEmail = (EditText)findViewById(R.id.emailLabel);
+        mPassword = (EditText)findViewById(R.id.passwordLabel);
+        mPhone = (EditText)findViewById(R.id.phoneLabel);
+        mName = (EditText)findViewById(R.id.nameLabel);
 
-        disableEditText(mEmail); 
-        disableEditText(mPassword); 
-        disableEditText(mPhone); 
+        disableEditText(mEmail);
+        disableEditText(mPassword);
+        disableEditText(mPhone);
         disableEditText(mName);
 
-        mEditButton = (android.widget.Button) findViewById(R.id.editButton); 
-        mEditButton.setOnClickListener(new View.OnClickListener() { 
-            public void onClick(View v) { 
-                enableEditText(mEmail); 
-                enableEditText(mPassword); 
-                enableEditText(mPhone); 
-                enableEditText(mName); 
-            } 
+        mEditButton = (Button) findViewById(editButton);
+        mEditButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                mSaveButton.setVisibility(View.VISIBLE);
+                enableEditText(mEmail);
+                enableEditText(mPassword);
+                enableEditText(mPhone);
+                enableEditText(mName);
+            }
         });
+
+        mSaveButton = (Button) findViewById(R.id.saveButton);
+        mSaveButton.setVisibility(View.GONE);
+        mSaveButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                mSaveButton.setVisibility(View.GONE);
+                disableEditText(mEmail);
+                disableEditText(mPassword);
+                disableEditText(mPhone);
+                disableEditText(mName);
+            }
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    private void disableEditText(android.widget.EditText editText) { 
-        editText.setFocusable(false); 
-        editText.setEnabled(false); 
-        editText.setCursorVisible(false); 
-        editText.setBackgroundColor(android.graphics.Color.TRANSPARENT); 
-    }  
+    private void disableEditText(EditText editText) {
+        editText.setFocusable(false);
+        editText.setEnabled(false);
+        editText.setBackgroundColor(Color.TRANSPARENT);
+    }
 
-    private void enableEditText(android.widget.EditText editText) { 
-        editText.setFocusable(true); 
-        editText.setEnabled(true); 
-        editText.setCursorVisible(true); 
+    private void enableEditText(EditText editText) {
+        editText.setFocusable(true);
+        editText.setEnabled(true);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder().setName("Profile Page").setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+                /*
+                TODO: Define a title for the content shown.
+                TODO: Make sure this auto-generated URL is correct.
+                */
+
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+    class Profile {
+        String email, password, phone, name;
     }
 }
