e0066f85a3a0619d530c65e023cf4f22317a3c20
Tinyen Shih <tinyensh@usc.edu>
2016-11-07 13:03:33 -0800

    Added some code in my calendar that checks validity of date. Removed all mockito compilations. MyCalendarTest a little more complete.

--- ParkHere/app/src/test/java/com/pfp/parkhere/MyCalendarTest.java
+++ ParkHere/app/src/test/java/com/pfp/parkhere/MyCalendarTest.java
@@ -4,10 +4,7 @@ import android.content.Context;
 
 import org.junit.Test;
 
-import static org.mockito.Mockito.*;
 import static org.junit.Assert.assertEquals;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import ObjectClasses.MyCalendar;
 
@@ -16,22 +13,46 @@ import ObjectClasses.MyCalendar;
  */
 
 public class MyCalendarTest {
-    //private static final String FAKE_STRING = "HELLO WORLD";
-
-    @Mock
-    Context mMockContext;
 
     @Test
     public void myCalendarConstructorTest() {
-        // Y2k
-        MyCalendar calendarUnderTest = new MyCalendar(2000,1,1,0,0);
+        // near Y2k
+        MyCalendar calendarUnderTest = new MyCalendar(2000,1,2,4,30);
 
         // ...when the string is returned from the object under test...
-        int result = calendarUnderTest.getYear();
+        int resultMonth = calendarUnderTest.getMonth();
+        int resultYear = calendarUnderTest.getYear();
+        int resultDay = calendarUnderTest.getDay();
+        int resultHour = calendarUnderTest.getHour();
+        int resultMin = calendarUnderTest.getMinute();
 
         // ...then the result should be the expected one.
         //assertThat(result, is(FAKE_STRING));
-        assertEquals(1,result);
+        assertEquals(1,resultMonth);
+        assertEquals(2000,resultYear);
+        assertEquals(2,resultDay);
+        assertEquals(4,resultHour);
+        assertEquals(30,resultMin);
+    }
+    @Test
+    public void myCalendarInvalidInputConstructorTest() {
+        // near Y2k
+        MyCalendar calendarUnderTest = new MyCalendar(-2000,13,300, 27,700);
+
+        // ...when the string is returned from the object under test...
+        int resultMonth = calendarUnderTest.getMonth();
+        int resultYear = calendarUnderTest.getYear();
+        int resultDay = calendarUnderTest.getDay();
+        int resultHour = calendarUnderTest.getHour();
+        int resultMin = calendarUnderTest.getMinute();
+
+        // ...then the result should be the expected one.
+        //assertThat(result, is(FAKE_STRING));
+        assertEquals(0,resultMonth);
+        assertEquals(-2000,resultYear);
+        assertEquals(0,resultDay);
+        assertEquals(0,resultHour);
+        assertEquals(0,resultMin);
     }
 
 }
