08a376121e94dc94e939434fb02ae0a195cd695d
Tinyen Shih <tinyensh@usc.edu>
2016-11-12 10:53:06 -0800

    Added labels to clarify my booking details's rating and reviews are for the space, not the owner Reviews are now a list view Added a nonfunctional button to go to Owner details Added a nonfunctional button to go to Rate and Review. Only appears if the variable "bookingDone" is set to true. MyBookingsDetails is now a scroll view to accommodate the additional elements on the activity

--- ParkHere/app/src/main/java/com/pfp/parkhere/MyBookingsDetailsActivity.java
+++ ParkHere/app/src/main/java/com/pfp/parkhere/MyBookingsDetailsActivity.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class MyBookingsDetailsActivity extends Activity{
     private String bookingIdentifier, ownerEmail, spaceName;
-
+    private String[] viewValues;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,13 +38,20 @@ public class MyBookingsDetailsActivity extends Activity{
         nameView.setText(extras.getString("SPACE_OWNERNAME"));
         TextView emailView = (TextView) findViewById(R.id.owner_email);
         emailView.setText(ownerEmail);
-        RatingBar rateBar = (RatingBar) findViewById(R.id.BookingDetailRatingBar);
+        RatingBar rateBar = (RatingBar) findViewById(R.id.SpaceAverageRating);
         DrawableCompat.setTint(rateBar.getProgressDrawable(), Color.parseColor("#FFCC00"));
         rateBar.setRating(extras.getInt("SPACE_RATING"));
-        TextView reviewView = (TextView) findViewById(R.id.space_review);
-        String spaceReview = "Review: " + extras.getString("SPACE_REVIEW");
-        reviewView.setText(spaceReview);
+        //String spaceReview = "Review: " + extras.getString("SPACE_REVIEW");
+        //reviewView.setText(spaceReview);
+        mockReviews();
+        //populateReviews();
 
+        //Determines whether to show "Rate and Review" button
+        boolean bookingDone = true;
+        if(bookingDone){
+            View rate_Review_Button = findViewById(R.id.Rate_Review_BookDetails);
+            rate_Review_Button.setVisibility(View.VISIBLE);
+        }
         bookingIdentifier = extras.getString("BOOKING_IDENTIFIER");
     }
 
@@ -57,6 +66,34 @@ public class MyBookingsDetailsActivity extends Activity{
         intent.putExtra("SPACE_NAME", spaceName);
         startActivity(intent);
     }
-
-
+    //This function opens the Owner details of this space
+    public void GoToOwnerDetail(View view){
+        //ownerEmail;
+        //Intent intent = new Intent(this, Activity.class);
+        //startActivity(intent);
+    }
+    //Creates Mock Reviews for app
+    public void mockReviews(){
+        ListView reviewsView = (ListView) findViewById(R.id.space_reviews);
+        viewValues = new String [11];
+        for(int i = 0; i < 11;i++){
+            viewValues[i] = "Review # " + i + " Blahblahblah car got towed away.";
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, viewValues);
+        // Assign adapter to ListView
+        reviewsView.setAdapter(adapter);
+    }
+    //Used in actual app
+//    public void populateReviews(){
+//    ListView reviewsView = (ListView) findViewById(R.id.space_reviews);
+//        viewValues = new String [sizeOfReviewList];
+//        for(int i = 0; i < sizeOfReviewList;i++){
+//            viewValues[i] = reviewList[i];
+//        }
+//    }
+    public void GoToRateAndReview(View view){
+        //Intent intent = new Intent(this, Activity.class);
+        //startActivity(intent);
+    }
 }
