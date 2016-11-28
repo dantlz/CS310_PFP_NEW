d72dee8f81bff2371145848f3b126774cac7acd6
Tianlin Zhang <tianlinz@dantlz.local>
2016-11-28 10:35:59 -0800

    AddPost and EditPost incorporated not tested

--- ParkHere/app/src/main/java/com/pfp/parkhere/EditPostActivity.java
+++ ParkHere/app/src/main/java/com/pfp/parkhere/EditPostActivity.java
@@ -1,7 +1,5 @@
 package com.pfp.parkhere;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,31 +9,30 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-import com.google.android.gms.vision.text.Text;
-
 import java.util.ArrayList;
 
 import ObjectClasses.CancellationPolicy;
 import ObjectClasses.MyCalendar;
-import ObjectClasses.Post;
-import ObjectClasses.Space;
 
 public class EditPostActivity extends AppCompatActivity {
 
-    Post currPost;
+    private String ownerEmail, spaceName, postName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_post);
 
-        currPost = populatePost();
+        postName = getIntent().getExtras().getString("POST_NAME");
+        ownerEmail = getIntent().getExtras().getString("SPACE_OWNEREMAIL");
+        spaceName = getIntent().getExtras().getString("SPACE_NAME");
 
-        TextView postName = (TextView)findViewById(R.id.view_name_edit_post);
-        postName.setText(currPost.getPostName());
+        TextView postNameField = (TextView)findViewById(R.id.view_name_edit_post);
+        postNameField.setText(postName);
 
         TextView postSpaceName = (TextView)findViewById(R.id.view_spaceName_edit_post);
-        postSpaceName.setText("From Space: " + currPost.getParentSpaceName());
+        postSpaceName.setText("From Space: " + spaceName);
+
 
         Spinner postCancellationPolicies = (Spinner)findViewById(R.id.cancellation_spinner);
         ArrayList<String> cancellationPolicies = new ArrayList<>();
@@ -46,60 +43,45 @@ public class EditPostActivity extends AppCompatActivity {
                 cancellationPolicies);
         postCancellationPolicies.setAdapter(policyAdapter);
 
-
     }
 
-    private Post populatePost() {
-        Post retPost = new Post();
-
-        retPost.setPostName("USC Colosseum");
-        retPost.setParentOwnerEmail("bradfora@usc.edu");
-        retPost.setParentSpaceName("USC Parking");
-        retPost.setPolicy(CancellationPolicy.MODERATE);
-
-        MyCalendar startDate = new MyCalendar(2016, 11, 20, 10, 30);
-        MyCalendar endDate = new MyCalendar(2016, 11, 30, 5, 00);
-
-        retPost.setAvailableStartDateAndTime(startDate);
-        retPost.setAvailableEndDateAndTime(endDate);
-
-        return retPost;
-
-    }
-
-    public void goToEditSpace(View view) {
-        Intent intent = new Intent(EditPostActivity.this, EditListedSpacesActivity.class);
-        intent.putExtra("SPACE_OWNEREMAIL", currPost.getParentOwnerEmail());
-        intent.putExtra("SPACE_NAME", currPost.getParentSpaceName());
-        startActivity(intent);
+    public void deletePost(View view) {
+        Global.posts().child(Global.reformatEmail(ownerEmail)).child(spaceName).child(postName).removeValue();
+        Global.spaces().child(Global.reformatEmail(ownerEmail)).child(spaceName).child("PostNames").child(postName).removeValue();
+        //TODO Deleting a post must delete bookings too, or it can't be deleted until bookings are done.
         finish();
     }
 
     public void savePostChanges(View view) {
         Spinner cancellationSpinner = (Spinner)findViewById(R.id.cancellation_spinner);
-        currPost.setPolicy(CancellationPolicy.valueOf(cancellationSpinner.getSelectedItem().toString().toUpperCase()));
+        Global.posts().child(Global.reformatEmail(ownerEmail)).child(spaceName).child(postName).child("policy").
+                setValue(CancellationPolicy.valueOf(cancellationSpinner.getSelectedItem().toString().toUpperCase()));
 
         DatePicker startDate = (DatePicker)findViewById(R.id.edit_post_start_date);
         TimePicker startTime = (TimePicker)findViewById(R.id.edit_post_start_time);
-        currPost.setAvailableStartDateAndTime(new MyCalendar(
+        MyCalendar startDateTime = new MyCalendar(
                 startDate.getYear(),
                 startDate.getMonth(),
                 startDate.getDayOfMonth(),
                 startTime.getHour(),
                 startTime.getMinute()
-        ));
+        );
+        Global.posts().child(Global.reformatEmail(ownerEmail)).child(spaceName).child(postName).child("availableStartDateAndTime").
+                setValue(startDateTime);
+
 
         DatePicker endDate = (DatePicker)findViewById(R.id.edit_post_end_date);
         TimePicker endTime = (TimePicker)findViewById(R.id.edit_post_end_time);
-        currPost.setAvailableEndDateAndTime(new MyCalendar(
+        MyCalendar endDateTime = new MyCalendar(
                 endDate.getYear(),
                 endDate.getMonth(),
                 endDate.getDayOfMonth(),
                 endTime.getHour(),
                 endTime.getMinute()
-        ));
+        );
+        Global.posts().child(Global.reformatEmail(ownerEmail)).child(spaceName).child(postName).child("availableEndDateAndTime").
+                setValue(endDateTime);
 
-        startActivity(new Intent(EditPostActivity.this, MapsActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
         finish();
     }
 }
